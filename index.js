1247515b481169b317315359cac4244e4ce9593a
Pedro <pfcittolin@gmail.com>
2021-05-19 20:25:14 -0300

    Starts implementing part B

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //@ts-check
 const net = require('net');
+const fs = require('fs/promises')
 const port = 4321;
 const host = '127.0.0.1';
 
@@ -13,19 +14,45 @@ if (cluster.isMaster) {
     }
   
     cluster.on('exit', function(worker, code, signal) {
-      console.log('worker ' + worker.process.pid + ' died');
+      //console.log('worker ' + worker.process.pid + ' died');
     });
   } else {
     const server = net.createServer();
     server.listen(port, host, () => {
-        console.log('TCP Server is running on port ' + port + '.');
+        //console.log('TCP Server is running on port ' + port + '.');
     });
 
     server.on('connection', (sock) => {
-        console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+        //console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
 
-        sock.on('data', function(data) {
-            console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        sock.on('data', async function(data) {
+            let dataStr = data.toString();
+            let tokenizedStr = dataStr.split(' ');
+            let fileStr = '.' + tokenizedStr[1];
+
+            let fileExists = true;
+            
+            let file = await fs.readFile(fileStr,'utf8')
+            .catch((error) => {
+                fileExists = false;
+            });
+
+            let statusLine = null;
+            let contentTypeLine = null;
+            let entityBody = null;
+
+            if (fileExists) {
+
+            } else {
+                statusLine = '404 Not Found';
+                entityBody = "<HTML>" +
+                            "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
+                            "<BODY>Not Found</BODY></HTML>";
+
+            }
+
+            
+            
         });
 
         sock.on('close', function(data) {
